ca7447825e8f946d7d41b39c07f74b1621104f42
hgKang02 <68604896+hgkang-tech@users.noreply.github.com>
2022-10-09 00:23:09 -0500

    Time: 3075 ms (34.03%), Space: 23.3 MB (19.09%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,53 +1,21 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         
-        temps = [None for i in range(len(nums))]
+        if nums == []:
+            return[]
         
-        self.merge_sort(nums, 0, len(nums) - 1, temps)
+        if len(nums) == 1:
+            return nums
         
-        return nums
-        
-    def merge_sort(self, nums, start, end, temps):
-        if start >= end:
-            return
-        
-        
-        mid = start + (end - start) // 2
-        
-        
-        self.merge_sort(nums, start, mid, temps)
-        self.merge_sort(nums, mid + 1, end, temps)
-        
-        return self.merge(nums, start, mid, end, temps)
-    
-    
-    def merge(self, nums, start, mid, end, temps):
-        
-        index = start
-
-        i = start
-        j = mid + 1
-        
-        while i <= mid and j <= end:
-            if nums[i] <= nums[j]:
-                temps[index] = nums[i] 
-                i += 1
+        pivot = random.choice(nums)
+        left, mid, right = [], [], []
+        for i in nums:
+            if i < pivot:
+                left.append(i)
+            
+            elif i > pivot:
+                right.append(i)
             else:
-                temps[index] = nums[j] 
-                j += 1            
-            
-            index += 1
-                
-        while i <= mid:
-            temps[index] = nums[i]
-            i += 1
-            index += 1
-                
-        while j <= end:
-            temps[index] = nums[j]
-            j += 1
-            index += 1
-            
-        for i in range(start, end + 1):
-            nums[i] = temps[i]
+                mid.append(i)
+        return self.sortArray(left) + mid + self.sortArray(right)
         
