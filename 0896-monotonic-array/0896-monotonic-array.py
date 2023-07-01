94a522962d4b5dce963082cac7545812e8fadb4f
hgKang02 <68604896+hgkang-tech@users.noreply.github.com>
2023-07-01 11:19:36 -0500

    Time: 906 ms (44.74%), Space: 24.1 MB (56.84%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -4,26 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        if len(nums) == 1 or len(nums) == 2:
-            return True
-        else:
-            if nums[0] < nums[1]:
-                mono = 'i'
-            elif nums[0] > nums[1]:
-                mono = 'd'
-            else:
-                mono = 's'
-            for i in range(1, len(nums) - 1, 1):
-                if mono == 'i' and nums[i] > nums[i + 1]:
-                    return False
-                elif mono == 'd' and nums[i] < nums[i + 1]:
-                    return False
-                elif mono == 's':
-                    if nums[i] < nums[i + 1]:
-                        mono = 'i'
-                    elif nums[i] > nums[i + 1]:
-                        mono = 'd'
-            return True
+        inc = dec = True
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                inc = False
+            elif nums[i] < nums[i + 1]:
+                dec = False
+        
+        return inc or dec
                     
             
         
