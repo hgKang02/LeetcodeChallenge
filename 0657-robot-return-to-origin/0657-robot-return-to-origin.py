f28366d5cfa82f30b29008a84e1898c47d017fae
hgKang02 <68604896+hgkang-tech@users.noreply.github.com>
2023-07-03 13:42:27 -0500

    Time: 69 ms (27.51%), Space: 13.3 MB (99.21%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        arr = [0,0]
+        for i in moves:
+            if i == 'U':
+                arr[0] -= 1
+            elif i == 'D':
+                arr[0] += 1
+            elif i == 'L':
+                arr[1] -= 1
+            elif i == 'R':
+                arr[1] += 1
+        if arr[0] == 0 and arr[1] == 0:
+            return True
+        return False
+        
